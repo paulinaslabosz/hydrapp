f0b86c31addcb2179d67af6ecfa00c6f44c6d102
Paulina Słabosz <sejmipool@gmail.com>
2021-03-31 20:29:00 +0200

    info

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,18 @@
 import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
-registerSW();
+//import {registerSW} from './pwa.js';
+//registerSW();
 
 /* place your code below */
+
 
 const addWater = document.querySelector(".add--js");
 const deleteWater = document.querySelector(".delete--js");
 const numberGlasses = document.querySelector(".glasses--js");
 const key = new Date().toLocaleString().slice(0, 10);
 const localStorageValue = localStorage.getItem(key);
+const info = document.querySelector('.info--js');
 
 let currentWater = 0;
 
@@ -33,5 +35,9 @@ deleteWater.addEventListener("click", () => {
   }
   numberGlasses.innerHTML = currentWater;
   localStorage.setItem(key, currentWater);
+});
+
+info.addEventListener("click", () => {
+  alert("Picie wody to niezwykle ważny element zdrowego odżywiania, pamiętaj o tym! Należy pić codziennie co najmniej 1.5 litra wody!");
 });
 
